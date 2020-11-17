28a51646aa0852a31a6c717f3e0c3ab2e2f2dc7c
Kento Wakamura <wakaken@notespace.jp>
2020-11-17 09:45:09 +0900

    再度マイグレーション実施（item、userを生成）

--- src/migrations/1605526166474-create-item.ts
+++ src/migrations/1605526166474-create-item.ts
@@ -1,13 +1,15 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateItem1605236377805 implements MigrationInterface {
-    name = 'CreateItem1605236377805'
+export class createItem1605526166474 implements MigrationInterface {
+    name = 'createItem1605526166474'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `item` (`id` int NOT NULL AUTO_INCREMENT, `todo` varchar(255) NOT NULL, `done` tinyint NOT NULL DEFAULT 0, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `age` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP TABLE `user`");
         await queryRunner.query("DROP TABLE `item`");
     }
 
